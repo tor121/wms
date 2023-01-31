681edd001d8eeefae24b8ba0a58b34d01ebb21d5
Polina <kaminskaia.p.a@gmail.com>
2023-01-31 16:39:51 +0300

    add mobile

--- js/lights.js
+++ js/lights.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  function myFunction(x) {
+    if (x.matches) { // Если медиа запрос совпадает
+      containerGar.style.paddingBottom = '0px';
+      container.style.paddingBottom = '1px';
+    } else {
+      containerGar.style.paddingBottom = '70px';
+    }
+  }
+  const x = window.matchMedia("(max-width: 768px)")
+
   const container = document.querySelector('.lights__container');
   const btn = document.querySelector('.lights__btn');
   const allItems = document.querySelectorAll('.lights__item');
@@ -7,7 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
       item.style.display = 'flex';
     });
     this.style.display = 'none';
-    container.style.paddingBottom = '70px';
+    myFunction(x);
+    // container.style.paddingBottom = '70px';
   });
 
   const containerGar = document.querySelector('.garden__container');
@@ -17,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     allItemsGar.forEach((item) => {
       item.style.display = 'flex';
     });
-    containerGar.style.paddingBottom = '70px';
+    // containerGar.style.paddingBottom = '70px';
     this.style.display = 'none';
   });
 });
