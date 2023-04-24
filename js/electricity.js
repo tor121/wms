8590c0ecaffc7384e69f80ada85799b39ecd39cd
tor121 <121507300+tor121@users.noreply.github.com>
2023-04-25 00:33:13 +0300

    Add files via upload

---
+++ js/electricity.js
@@ -0,0 +1,43 @@
+const btnElec = document.querySelector('.electricity__btn');
+const containerElec = document.querySelector('.electricity');
+const allItemsElec = document.querySelectorAll('.electricity__paragraph');
+const allItemsElecVisibility = document.querySelectorAll('.electricity__change-visibility');
+
+const addActiveContainerElec = () => {
+  containerElec.classList.add('active');
+};
+
+const makeVision = () => {
+  allItemsElecVisibility.forEach((item) => {
+    item.style.display = 'block';
+  });
+  console.log(allItemsElec)
+}
+
+const makeHide = () => {
+  allItemsElecVisibility.forEach((item) => {
+    item.style.display = 'none';
+  });
+  btnElec.style.display = 'block';
+}
+
+btnElec.addEventListener('click', (e) => {
+  e.stopPropagation();
+
+  makeVision();
+  addActiveContainerElec();
+  e.currentTarget.style.display = 'none';
+});
+
+document.addEventListener('click', (e) => {
+  const { target } = e;
+  const isContainerElec = target == containerElec || containerElec.contains(target);
+  const isActive = containerElec.classList.contains('active');
+
+  if (!isContainerElec && isActive) {
+    containerElec.classList.remove('active');
+    makeHide();
+
+  }
+});
+
