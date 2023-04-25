dfe2b92a5f73561dfb9fe867a31f08cd885cb706
tor121 <121507300+tor121@users.noreply.github.com>
2023-04-25 15:09:53 +0300

    Add files via upload

--- js/security.js
+++ js/security.js
@@ -1,43 +1,44 @@
-const btnElec = document.querySelector('.security__btn');
-const containerElec = document.querySelector('.security');
-const allItemsElec = document.querySelectorAll('.security__paragraph');
-const allItemsElecVisibility = document.querySelectorAll('.security__change-visibility');
+document.addEventListener('DOMContentLoaded', () => {
+const btnSecu = document.querySelector('.security__btn');
+const containerSecu = document.querySelector('.security');
+const allItemsSecu = document.querySelectorAll('.security__paragraph');
+const allItemsSecuVisibility = document.querySelectorAll('.security__change-visibility');
 
-const addActiveContainerElec = () => {
-  containerElec.classList.add('active');
+const addActiveContainerSecu = () => {
+  containerSecu.classList.add('active');
 };
 
 const makeVision = () => {
-  allItemsElecVisibility.forEach((item) => {
+  allItemsSecuVisibility.forEach((item) => {
     item.style.display = 'block';
   });
-  console.log(allItemsElec)
+  console.log(allItemsSecu)
 }
 
 const makeHide = () => {
-  allItemsElecVisibility.forEach((item) => {
+  allItemsSecuVisibility.forEach((item) => {
     item.style.display = 'none';
   });
-  btnElec.style.display = 'block';
+  btnSecu.style.display = 'block';
 }
 
-btnElec.addEventListener('click', (e) => {
+btnSecu.addEventListener('click', (e) => {
   e.stopPropagation();
 
   makeVision();
-  addActiveContainerElec();
+  addActiveContainerSecu();
   e.currentTarget.style.display = 'none';
 });
 
 document.addEventListener('click', (e) => {
   const { target } = e;
-  const isContainerElec = target == containerElec || containerElec.contains(target);
-  const isActive = containerElec.classList.contains('active');
+  const isContainerSecu = target == containerSecu || containerSecu.contains(target);
+  const isActive = containerSecu.classList.contains('active');
 
-  if (!isContainerElec && isActive) {
-    containerElec.classList.remove('active');
+  if (!isContainerSecu && isActive) {
+    containerSecu.classList.remove('active');
     makeHide();
 
   }
 });
-
+})
